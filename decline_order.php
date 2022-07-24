aa189b42b11d208c749f64606c896bc61cd1332f
DevarshMehra <65247066+DevarshMehra@users.noreply.github.com>
2022-07-24 17:50:13 +0530

    Delete decline_order.php

--- decline_order.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$connection = new mysqli("localhost", "root", "", "onlie_store_db");
-$sqlCommand = $connection->prepare("delete from temporary_place_order where email=?");
-$sqlCommand->bind_param("s", $_GET["email"]);
-$sqlCommand->execute();
-
